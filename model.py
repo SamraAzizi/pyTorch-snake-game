1f5e83fde3b46f85802c457f030afb7cf621ed9b
SamraAzizi <samarraazizi@gmail.com>
2025-11-19 22:12:32 +0500

    snake game of pyTorch and pygame

--- model.py
+++ model.py
@@ -68,3 +68,25 @@ class SnakeGame:
                     self.direction = Direction.UP
                 elif event.key == pygame.K_DOWN:
                     self.direction = Direction.DOWN
+                    # 2. move
+        self._move(self.direction) # update the head
+        self.snake.insert(0, self.head)
+        
+        # 3. check if game over
+        game_over = False
+        if self._is_collision():
+            game_over = True
+            return game_over, self.score
+            
+        # 4. place new food or just move
+        if self.head == self.food:
+            self.score += 1
+            self._place_food()
+        else:
+            self.snake.pop()
+        
+        # 5. update ui and clock
+        self._update_ui()
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        return game_over, self.score
