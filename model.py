3df33384b46292e1eb88100d789706b9cb7d7188
SamraAzizi <samarraazizi@gmail.com>
2025-11-21 12:52:28 +0500

    snake game of pyTorch and pygame the model and pytorch

--- model.py
+++ model.py
@@ -38,4 +38,23 @@ class QTrainer:
         next_state = torch.tensor(next_state, dtype=torch.float)
         action = torch.tensor(action, dtype=torch.long)
         reward =torch.tensor(reward, dtype=torch.float)
-        
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+
+            #predict Q values with current state
+
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward[idx]
+            if not done[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+            
